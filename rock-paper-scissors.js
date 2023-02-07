6d22e8bd54f21c94a6c5f16203631be51a14152c
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-06 20:22:16 -0700

    Add game logic and win loss conditions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -26,5 +26,46 @@ function getUserChoice(){
     }
 }
 
+//Function to choose winner
+//while win count for either computer or user is under 3 
+//display current score
+//display selections
+//loop through choice comaparison
+//tally win counts for user and computer
+//when user or computer reach 3 wins display win/loss message
+
+function chooseWinner(){
+    let userWinCount = 0; 
+    let computerWinCount = 0;
+    while(userWinCount < 3 && computerWinCount < 3){
+        console.log(`The current score is User ${userWinCount}, Computer ${computerWinCount}`);
+        let user = getUserChoice();
+        let computer = getComputerChoice();
+        console.log(`user picks ${user}`);
+        console.log(`computer picks ${computer}`);
+        if(user === computer){
+            console.log("It's a tie!");  
+        }else if (user === "rock" && computer != "paper"){
+            userWinCount += 1;
+            console.log("You win!");  
+        }else if(user === "paper" && computer != "scissors"){
+            console.log("You win!");
+            userWinCount += 1;  
+        }else if(user === "scissors" && computer != "rock"){
+            console.log("You win!");
+            userWinCount += 1;
+        }else{
+            console.log("You lose!");
+            computerWinCount += 1;
+        }
+    } 
+    console.log(`The final score is User ${userWinCount}, Computer ${computerWinCount}`);
+    let playAgain = userWinCount > computerWinCount ? confirm("Congratulations, you win! Play again?") : confirm("You lose, better luck next time! Play again?");
+    playAgain ? chooseWinner() : alert("See you next time!");
+    
+}
+
+chooseWinner();
+
 
 
