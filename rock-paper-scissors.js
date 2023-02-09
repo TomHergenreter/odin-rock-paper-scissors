df991b1d61d1d6f31933f785a42113b9e9d00f69
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-08 21:17:57 -0700

    Add display score and display selection functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,7 +6,7 @@ let computerWinCount = 0;
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', getUserChoice));
 
-//Get user selection
+//Get user selection and initialize round
 function getUserChoice(e){
     playRound(this.id);
     return;
@@ -19,35 +19,53 @@ function getComputerChoice (){
     return computerChoice;
 }
 
-//Update score 
-function updateScore(){
+//Compare choices and update score
+function chooseWinner(userChoice, computerChoice){
+    let results = ''   
+    if(userChoice === computerChoice){
+        results = "It's a tie!";  
+    }else if (userChoice === "rock" && computerChoice != "paper"){
+        userWinCount += 1;
+        results = "You Win!"; 
+    }else if(userChoice === "paper" && computerChoice != "scissors"){
+        userWinCount += 1;
+        results = "You Win!";   
+    }else if(userChoice === "scissors" && computerChoice != "rock"){
+        userWinCount += 1;
+        results = "You Win!"; 
+    }else{
+        computerWinCount += 1;
+        results = "You Lose!"; 
+    }
+}
+
+//Update score board
+function updateScoreBoard(){
     let displayUserScore = document.querySelector('.user-score');
     displayUserScore.textContent = `Player: ${userWinCount}`;
     let displayComputerScore = document.querySelector('.computer-score');
     displayComputerScore.textContent = `Computer: ${computerWinCount}`;
 }
 
+//Display user and computer selections
+function displayChoice(userChoice, computerChoice){
+    let displayUserScore = document.querySelector('.user-selection');
+    displayUserScore.textContent = `Player Selects: ${userChoice}`;
+    let displayComputerScore = document.querySelector('.computer-selection');
+    displayComputerScore.textContent = `Computer Selects: ${computerChoice}`;
+}
+
+function endGame(){
+    alert('it works')
+}
+
 //Compare selections, determine winner, tally score, check if win/loss condition is met
 function playRound(userChoice){
-    let computer = getComputerChoice();
-    console.log(`user picks ${userChoice}`);
-    console.log(`computer picks ${computer}`);
-    if(userChoice === computer){
-        console.log("It's a tie!");  
-    }else if (userChoice === "rock" && computer != "paper"){
-        userWinCount += 1;
-        console.log("You win!");  
-    }else if(userChoice === "paper" && computer != "scissors"){
-        console.log("You win!");
-        userWinCount += 1;  
-    }else if(userChoice === "scissors" && computer != "rock"){
-        console.log("You win!");
-        userWinCount += 1;
-    }else{
-        console.log("You lose!");
-        computerWinCount += 1;
-    }
-    updateScore();
+    let computerChoice = getComputerChoice();
+    displayChoice(userChoice, computerChoice);
+    chooseWinner(userChoice, computerChoice);
+    updateScoreBoard();
+    if(userWinCount === 5 || computerWinCount === 5) endGame(); 
 }
 
 
