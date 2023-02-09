014be0e629c8f4fb42a01edb35f319a416e67365
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-08 20:18:04 -0700

    Refactor code to work with ui, add styles

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,6 @@
 //Odin Project - Rock, Paper, Scissors
 //Tom Hergenreter - Feb 2022
 
-//Initialize game
 let userWinCount = 0; 
 let computerWinCount = 0;
 const buttons = document.querySelectorAll('button');
@@ -20,37 +19,35 @@ function getComputerChoice (){
     return computerChoice;
 }
 
+//Update score 
+function updateScore(){
+    let displayUserScore = document.querySelector('.user-score');
+    displayUserScore.textContent = `Player: ${userWinCount}`;
+    let displayComputerScore = document.querySelector('.computer-score');
+    displayComputerScore.textContent = `Computer: ${computerWinCount}`;
+}
+
 //Compare selections, determine winner, tally score, check if win/loss condition is met
 function playRound(userChoice){
-    console.log(`The current score is User ${userWinCount}, Computer ${computerWinCount}`);
-    let user = userChoice;
     let computer = getComputerChoice();
-    console.log(`user picks ${user}`);
+    console.log(`user picks ${userChoice}`);
     console.log(`computer picks ${computer}`);
-    if(user === computer){
+    if(userChoice === computer){
         console.log("It's a tie!");  
-    }else if (user === "rock" && computer != "paper"){
+    }else if (userChoice === "rock" && computer != "paper"){
         userWinCount += 1;
         console.log("You win!");  
-    }else if(user === "paper" && computer != "scissors"){
+    }else if(userChoice === "paper" && computer != "scissors"){
         console.log("You win!");
         userWinCount += 1;  
-    }else if(user === "scissors" && computer != "rock"){
+    }else if(userChoice === "scissors" && computer != "rock"){
         console.log("You win!");
         userWinCount += 1;
     }else{
         console.log("You lose!");
         computerWinCount += 1;
     }
-
-    if(userWinCount === 3 || computerWinCount === 3){winLossMessage(userWinCount, computerWinCount)};
-}
-
-//Display win/loss message and score, ask for rematch
-function winLossMessage(userWinCount, computerWinCount){
-    console.log(`The final score is User ${userWinCount}, Computer ${computerWinCount}`);
-    let playAgain = userWinCount > computerWinCount ? confirm("Congratulations, you win! Play again?") : confirm("You lose, better luck next time! Play again?");
-    playAgain ? playRound() : alert("See you next time!");
+    updateScore();
 }
 
 
